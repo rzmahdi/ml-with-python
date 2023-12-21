5ed6b8833cae759675c0dbf493a0d7392e6c5853
mahdi <mahdirazmkhah45@gmail.com>
2023-12-21 15:52:06 +0330

    add function "Spliter" to remove comma in data

--- read_data.py
+++ read_data.py
@@ -1,13 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-# for i in range(1, 333):
-data = requests.get('https://www.truecar.com/used-cars-for-sale/listings/?page=2')
-soup = BeautifulSoup(data.text, 'html.parser')
-cards = soup.find('div', 'card-content')
-top = cards.find('div', 'vehicle-card-top').find('div', 'truncate')
-price = cards.find('div', 'vehicle-card-bottom-pricing').find('div', 'normal-case').text
+# split comma in string
+def spliter(string:str):
+    if ',' in string:
+        return int("".join(string.split(',')))
+    else:
+        return int(string)
 
-year = top.find('span', 'vehicle-card-year').text
-car_name, model = top.find('span', 'truncate').text.split()
-print(soup)
+# read data from truecar.com
+for i in range(1, 333):
+    data = requests.get(f'https://www.truecar.com/used-cars-for-sale/listings/?page={i}')
+    soup = BeautifulSoup(data.text, 'html.parser')
+    
+    cards = soup.find('div', 'card-content')
+    top = cards.find('div', 'vehicle-card-top').find('div', 'truncate')
+
+    year = int(top.find('span', 'vehicle-card-year').text)
+    car_name, model = top.find('span', 'truncate').text.split()
+    price = spliter(cards.find('div', 'vehicle-card-bottom-pricing').find('div', 'normal-case').text.split("$")[-1])
+    mile_age = spliter(cards.find('div', 'border-t').find('div', 'truncate').text.split()[0])
